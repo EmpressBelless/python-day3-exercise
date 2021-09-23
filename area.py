de5739a7ead4f93227d5c40302d67ef3cec233b6
Suzette W <78188677+EmpressBelless@users.noreply.github.com>
2021-09-23 09:27:47 -0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+def myfunc():
+   length = int(input("what is the length?: "))
+   width = int(input("what is the width?: "))
+   Area = length * width
+   return Area
+
+
+print(myfunc())
---
+++ circle.py
@@ -0,0 +1,7 @@
+def mycircle():
+  Pi = 3.14
+  r = float(input("enter the radius of a circle, the output will be the circumference of a circle: "))
+  Area = Pi * r * r
+  return Area
+  
+print(mycircle())
